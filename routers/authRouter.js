f2f4148bfd8df79dcbac36d5dcf9b399889651cf
Andrey Burov <AnBurov@Korusconsulting.ru>
2023-08-15 23:05:39 +0300

    a

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,71 +1,78 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const { User } = require('../db/models');
+const express = require('express')
+const bcrypt = require('bcrypt')
+const { User } = require('../db/models')
 
-const authRouter = express.Router();
+const authRouter = express.Router()
 
 authRouter.post('/signup', async (req, res) => {
-  try {
-    const { nickname, email, password } = req.body;
-    console.log(req.body);
+	try {
+		const { nickname, email, password } = req.body
 
-    const hashpass = await bcrypt.hash(password, 10);
-    const [foundUser, created] = await User.findOrCreate({
-      where: {email},
-      defaults: {
-        nickname,
-        hashpass,
-      },
-    });
+		const hashpass = await bcrypt.hash(password, 10)
+		const [foundUser, created] = await User.findOrCreate({
+			where: { email },
+			defaults: {
+				nickname,
+				hashpass
+			}
+		})
 
-    if (!created) return res.status(401).json({ message: 'Email is in use' });
+		if (!created)
+			return res.status(401).json({ message: 'Email is in use' })
 
-    req.session.user = foundUser;
+		const { id } = foundUser
+		const userWithoutPass = { id, nickname, email }
 
-    return res.json(foundUser);
-  } catch (e) {
-    console.log(e);
-  }
-});
+		req.session.user = userWithoutPass
+
+		return res.json(userWithoutPass)
+	} catch (e) {
+		console.log(e)
+	}
+})
 
 authRouter.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
+	try {
+		console.log('---------------,dfsaasd')
+		const { email, password } = req.body
 
-    const foundUser = await User.findOne({ where: { email } });
+		const foundUser = await User.findOne({ where: { email } })
 
-    if (!foundUser) return res.status(401).json({ message: 'No such email' });
+		if (!foundUser)
+			return res.status(401).json({ message: 'No such email' })
 
-    if (await bcrypt.compare(password, foundUser.hashpass)) {
-      req.session.user = foundUser;
-      return res.json(foundUser);
-    }
+		if (await bcrypt.compare(password, foundUser.hashpass)) {
+			const { id, nickname, email } = foundUser
+			const userWithoutPass = { id, nickname, email }
+			req.session.user = userWithoutPass
+			return res.json(userWithoutPass)
+		}
 
-    return res.status(401).json({ message: 'Wrong password' });
-  } catch (e) {
-    console.log(e);
-  }
-});
+		return res.status(401).json({ message: 'Wrong password' })
+	} catch (e) {
+		console.log(e)
+	}
+})
 
 authRouter.get('/logout', (req, res) => {
-  try {
-    req.session.destroy();
-    res.clearCookie('user_sid');
-    res.sendStatus(200);
-  } catch (e) {
-    console.log(e);
-  }
-});
+	try {
+		req.session.destroy()
+		res.clearCookie('user_sid')
+		res.sendStatus(200)
+	} catch (e) {
+		console.log(e)
+	}
+})
 
 authRouter.get('/check', async (req, res) => {
-  try {
-    if (req.session?.user?.id) {
-      return res.json(req.session.user);
-    }
-    return res.sendStatus(401);
-  } catch (e) {
-    console.log(e);
-  }
-});
+	try {
+		if (req.session?.user?.id) {
+			return res.json(req.session.user)
+		}
+		return res.sendStatus(401)
+	} catch (e) {
+		console.log(e)
+	}
+})
 
-module.exports = authRouter;
+module.exports = authRouter
