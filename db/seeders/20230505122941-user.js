bf8c21e3237fdbfcfbe37589d8373b46a84bb1e9
Andrey Burov <AnBurov@Korusconsulting.ru>
2023-12-26 02:37:08 +0300

    b

---
+++ db/seeders/20230505122941-user.js
@@ -0,0 +1,23 @@
+const { v4: uuidv4 } = require('uuid')
+const bcrypt = require('bcrypt')
+
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.bulkInsert(
+			'Users',
+			[
+				{
+					id: uuidv4(),
+					email: 'aaa@a',
+					nickname: 'aaa',
+					hashpass: `${await bcrypt.hash('123', 10)}`
+				}
+			],
+			{}
+		)
+	},
+
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete('Users', null, {})
+	}
+}
