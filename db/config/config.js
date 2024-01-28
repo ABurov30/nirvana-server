8b4a7ee0204c4f5ed8b2fa0d3d747f50be80defd
Andrey Burov <AnBurov@Korusconsulting.ru>
2024-01-28 22:38:03 +0300

    try deploy

--- db/config/config.js
+++ db/config/config.js
@@ -4,22 +4,16 @@ module.exports = {
 	development: {
 		username: 'andreyburov',
 		password: null,
-		database: process.env.DB_DATABASE,
-		host: process.env.DB_HOST,
-		dialect: 'postgres'
-	},
-	test: {
-		username: process.env.DB_USERNAME,
-		password: null,
-		database: process.env.DB_DATABASE,
-		host: process.env.DB_HOST,
+		database: 'nirvana',
+		host: '127.0.0.1',
 		dialect: 'postgres'
 	},
 	production: {
 		username: process.env.DB_USERNAME,
-		password: null,
+		password: process.env.DB_PASSWORD,
 		database: process.env.DB_DATABASE,
 		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
 		dialect: 'postgres'
 	}
 }
--- db/models/index.js
+++ db/models/index.js
@@ -10,6 +10,7 @@ const db = {}
 
 let sequelize
 if (config.use_env_variable) {
+	console.log(config, '-------------')
 	sequelize = new Sequelize(process.env[config.use_env_variable], config)
 } else {
 	sequelize = new Sequelize(
